9394fbdbe963b1d6831763abc797a3a0f10fdbcc
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-05 00:57:42 +0530

    Pin Hilt 2.58 for AGP 8 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.3.21" apply false
     id("org.jetbrains.kotlin.kapt") version "2.3.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
-    id("com.google.dagger.hilt.android") version "2.60.1" apply false
+    id("com.google.dagger.hilt.android") version "2.58" apply false
 }
