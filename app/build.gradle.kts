d10da0e3e5eda24c0ae797d5de836ffd3c5140e3
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-05 11:11:06 +0530

    chore: bump First Useful Day alpha to v0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.navin.personallifeos"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.5.0-setup-redesign-alpha"
+        versionCode = 6
+        versionName = "0.6.0-first-useful-day-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
