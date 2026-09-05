50b9d0b2200368a57c5c01d4b43ac63026c4e6c1
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-05 15:42:46 +0530

    feat: wire saved theme preference into app shell

--- app/src/main/java/com/navin/personallifeos/MainActivity.kt
+++ app/src/main/java/com/navin/personallifeos/MainActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.navin.personallifeos.ui.navigation.PersonalLifeOsRoot
 import com.navin.personallifeos.ui.theme.PersonalLifeOsTheme
+import com.navin.personallifeos.ui.viewmodel.AppEntryViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,8 +19,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PersonalLifeOsTheme {
-                PersonalLifeOsRoot()
+            val entryViewModel: AppEntryViewModel = hiltViewModel()
+            val themeMode by entryViewModel.themeMode.collectAsState()
+            val systemDark = isSystemInDarkTheme()
+            val darkTheme = when (themeMode) {
+                "dark" -> true
+                "light" -> false
+                else -> systemDark
+            }
+            PersonalLifeOsTheme(darkTheme = darkTheme) {
+                PersonalLifeOsRoot(entryViewModel = entryViewModel)
             }
         }
     }
